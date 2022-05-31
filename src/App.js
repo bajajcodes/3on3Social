be2e85df78bf3e42c773773687f021d8541e8f0c
ShubhamBajaj <shmbajaj32@gmail.com>
2022-05-31 22:43:37 +0530

    App Root Element

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Header, Footer, Navigation, Sidebar } from "components";
+import { checkIsNavigationAndSidebarRequired } from "utils";
+import { RouteSwitch } from "utils";
+import { useLocation } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+function App() {
+  const location = useLocation();
+
+  const isNavigationAndSidebarRequired = checkIsNavigationAndSidebarRequired(
+    location.pathname
+  );
+
+  return (
+    <div
+      className={`app bg-primary-background lg:grid-rows-[max-content_4fr_max-content] ${
+        !isNavigationAndSidebarRequired
+          ? "lg:grid-cols-[auto_4fr_auto]"
+          : "lg:grid-cols-[1fr_4fr_1fr]"
+      }`}
+    >
+      <Header />
+      {isNavigationAndSidebarRequired && <Navigation />}
+      <RouteSwitch />
+      {isNavigationAndSidebarRequired && <Sidebar />}
+      <Footer />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export { App };
