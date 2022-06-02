44e3836de847ce1c99c40985534e88603dabba98
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-02 17:14:53 +0530

    Auth Redirection Constrainet Added

--- src/utils/RouteSwitch.js
+++ src/utils/RouteSwitch.js
@@ -1,13 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { Landing, Home } from "pages";
-import { Signup } from "features";
+import { Signup, Login } from "features";
+import { CheckAuth } from "./CheckAuth";
 
 function RouteSwitch() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/signup"
+        element={
+          <CheckAuth>
+            <Signup />
+          </CheckAuth>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <CheckAuth>
+            <Login />
+          </CheckAuth>
+        }
+      />
       <Route path="*" element={<div className="main">Not Found</div>} />
     </Routes>
   );
