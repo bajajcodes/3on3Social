f6fb9b6a0b778903969ce07dfb9564d1f9572082
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-02 12:11:46 +0530

    Folder Renaming

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { Footer } from "./footer/Footer";
 export { Navigation } from "./navigation/Navigation";
 export { Sidebar } from "./sidebar/Sidebar";
 export { AppHeading } from "./appHeading/AppHeading";
-export { FormInput } from "./FormInput/FormInput";
-export { FormButton } from "./FormButton/FormButton";
+export { FormInput } from "./formInput/FormInput";
+export { FormButton } from "./formButton/FormButton";
