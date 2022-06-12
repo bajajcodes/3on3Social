7c31dd86d7168d428d83cf738561ea585699c04b
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-12 16:30:44 +0530

    Thunks For Follow/Unfollow Features

--- src/features/profile/profileSlice.thunks.js
+++ src/features/profile/profileSlice.thunks.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { db } from "firebaseLocal";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 const updatedUserProfile = createAsyncThunk(
   "thunk/updateUserProfile",
@@ -17,4 +17,40 @@ const updatedUserProfile = createAsyncThunk(
   }
 );
 
-export { updatedUserProfile };
+const followUser = createAsyncThunk(
+  "thunk/followUser",
+  async ({ following, follower }, { rejectWithValue }) => {
+    try {
+      let followerDocRef = doc(db, "users", follower.uid);
+      let followingDocRef = doc(db, "users", following.uid);
+      await updateDoc(followerDocRef, {
+        following: arrayUnion(following.username),
+      });
+      await updateDoc(followingDocRef, {
+        followers: arrayUnion(follower.username),
+      });
+    } catch (error) {
+      return rejectWithValue(error.message ?? "Error Message NA");
+    }
+  }
+);
+
+const unFollowUser = createAsyncThunk(
+  "thunk/unFollowUser",
+  async ({ following, follower }, { rejectWithValue }) => {
+    try {
+      let followerDocRef = doc(db, "users", follower.uid);
+      let followingDocRef = doc(db, "users", following.uid);
+      await updateDoc(followerDocRef, {
+        following: arrayRemove(following.username),
+      });
+      await updateDoc(followingDocRef, {
+        followers: arrayRemove(follower.username),
+      });
+    } catch (error) {
+      return rejectWithValue(error.message ?? "Error Message NA");
+    }
+  }
+);
+
+export { updatedUserProfile, followUser, unFollowUser };
