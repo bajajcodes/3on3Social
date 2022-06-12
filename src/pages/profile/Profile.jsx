1a8776845771e71d000fc01bf1001309d8f4bd27
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-12 16:35:09 +0530

    Code breakdown into custom hook

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,7 +3,7 @@ import {
   FollowButton,
   UnfollowButton,
 } from "./Profile.buttons";
-import { getDate } from "utils";
+import { useProfile } from "./Profile.hook";
 import {
   DisplayName,
   ProfilePhoto,
@@ -11,123 +11,26 @@ import {
   Loader,
   Post,
 } from "components";
-import { Toast } from "utils";
-import { db } from "firebaseLocal";
-import {
-  doc,
-  collection,
-  onSnapshot,
-  query,
-  orderBy,
-  where,
-} from "firebase/firestore";
-import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
 
 function Profile() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const uid = useSelector((state) => state.auth.uid);
-  const profileUid = location.pathname.split("/")[2];
-  const following =
-    useSelector((state) => state.profile.userInfo?.following) ?? [];
-  const [displayEditProfileModal, setDisplayEditProfileModal] = useState(false);
-  const [userInfoStatus, setUserInfoStatus] = useState("idle");
-  const [userInfo, setUserInfo] = useState({});
-  const [posts, setPosts] = useState([]);
-  const [postStatus, setPostStatus] = useState("idle");
-
-  function showEditProfileModal() {
-    setDisplayEditProfileModal(true);
-  }
-
-  function setDisplayEditProfileModalToInitialState() {
-    setDisplayEditProfileModal(false);
-  }
-
-  function followUser() {
-    if (uid) {
-      // * Will be removed post follow/unfollow code addition
-      console.log("Followed");
-    } else {
-      navigate("/login", { state: { from: `/profile/${profileUid}` } });
-    }
-  }
-
-  function unfollowUser() {
-    if (uid) {
-      // * Will be removed post follow/unfollow code addition
-      console.log("unfollowing");
-    } else {
-      navigate("/login", { state: { from: `/profile/${profileUid}` } });
-    }
-  }
-
-  useEffect(() => {
-    const uid = location.pathname.split("/")[2];
-    setUserInfoStatus("loading");
-    const unsubscribe = onSnapshot(
-      doc(db, "users", uid),
-      (snapshot) => {
-        setUserInfoStatus("success");
-        const userInfo = {
-          ...snapshot.data(),
-          createdAt: getDate(snapshot.data()),
-          uid: snapshot.id,
-        };
-        setUserInfo(userInfo);
-      },
-      (error) => {
-        Toast.error(error.message);
-        setUserInfoStatus("failed");
-      }
-    );
-    return () => {
-      unsubscribe();
-      setUserInfoStatus("idle");
-      setUserInfo({});
-    };
-  }, [location.pathname]);
-
-  useEffect(() => {
-    const uid = location.pathname.split("/")[2];
-    setPostStatus("loading");
-    const userPostsQuery = query(
-      collection(db, "posts"),
-      where("userId", "==", uid),
-      orderBy("createdAt", "desc")
-    );
-    const unsubscribe = onSnapshot(
-      userPostsQuery,
-      (querySnapshot) => {
-        const posts = [];
-        querySnapshot.forEach((q) =>
-          posts.push({
-            ...q.data(),
-            id: q.id,
-            createdAt: getDate(q.data()),
-          })
-        );
-        setPosts(posts);
-        setPostStatus("success");
-      },
-      (error) => {
-        Toast.error(error.message);
-        setPostStatus("failed");
-      }
-    );
-    return () => {
-      unsubscribe();
-      setPostStatus("idle");
-      setPosts([]);
-    };
-  }, [location.pathname]);
+  const {
+    uid,
+    following,
+    userInfo,
+    userInfoStatus,
+    posts,
+    postStatus,
+    dispatchFollowUser,
+    dispatchUnfollowUser,
+    displayEditProfileModal,
+    showEditProfileModal,
+    setDisplayEditProfileModalToInitialState,
+  } = useProfile();
 
   return (
     <>
       <main className="main grid gap-2 p-4 m-auto lg:mx-auto lg:my-0 z-0">
-        <section className="w-full max-w-xl lg:w-[36rem] grid gap-2 place-items-center text-center">
+        <section className="w-full max-w-xl grid gap-2 place-items-center text-center">
           {userInfoStatus === "success" &&
             postStatus === "success" &&
             userInfo?.username && (
@@ -144,12 +47,12 @@ function Profile() {
                   <EditProfileButton onClickHandler={showEditProfileModal} />
                 )}
                 {uid !== userInfo.uid &&
-                  following.indexOf(userInfo.uid) === -1 && (
-                    <FollowButton onClickHandler={followUser} />
+                  following.indexOf(userInfo.username) === -1 && (
+                    <FollowButton onClickHandler={dispatchFollowUser} />
                   )}
                 {uid !== userInfo.uid &&
-                  following.indexOf(userInfo.uid) !== -1 && (
-                    <UnfollowButton onClickHandler={unfollowUser} />
+                  following.indexOf(userInfo.username) !== -1 && (
+                    <UnfollowButton onClickHandler={dispatchUnfollowUser} />
                   )}
                 {userInfo?.bio && <p className="">{userInfo.bio}</p>}
                 {userInfo?.website && (
@@ -179,17 +82,19 @@ function Profile() {
               </>
             )}
 
-          <div className="relative">
-            {(userInfoStatus === "loading" || postStatus === "loading") && (
+          {(userInfoStatus === "loading" || postStatus === "loading") && (
+            <div className="relative">
               <Loader message="Profile Loading..." />
-            )}
-          </div>
+            </div>
+          )}
         </section>
-        <section className="w-full max-w-xl lg:w-[36rem] grid gap-2">
+        <section className="w-full max-w-xl grid gap-2">
           {postStatus === "success" &&
             userInfoStatus === "success" &&
             posts.length !== 0 && (
-              <h1 className="text-4xl font-medium">Your Posts</h1>
+              <h1 className="text-4xl font-medium">
+                {userInfo.uid === uid ? "Your Posts" : "Latest Posts"}
+              </h1>
             )}
           {postStatus === "success" && userInfoStatus === "success" && (
             <section>
@@ -205,7 +110,7 @@ function Profile() {
                       createdAt: post.createdAt,
                       content: post.content,
                       id: post.id,
-                      uid,
+                      uid: userInfo.uid,
                     }}
                   />
                 ))}
@@ -216,9 +121,11 @@ function Profile() {
               )}
             </section>
           )}
-          <div className="relative">
-            {postStatus === "loading" && <Loader message="Posts Loading..." />}
-          </div>
+          {postStatus === "loading" && (
+            <div className="relative">
+              <Loader message="Posts Loading..." />
+            </div>
+          )}
         </section>
       </main>
       {displayEditProfileModal && (
