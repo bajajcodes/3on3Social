2d0e560d27dc087f0c9bb7e2889345548f2332af
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-14 13:23:21 +0530

    Css Updated For 404 Page

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,11 +1,11 @@
 function NotFound() {
   return (
     <main className="main max-w-xl grid gap-2 p-4 m-auto lg:mx-auto lg:my-0 z-0">
-      <section className="w-full h-0 pb-[96.67%] relative">
+      <section className="w-full h-full">
         <img
           src="/assets/images/404.png"
           alt="404 Page Not Found"
-          className="w-full h-auto blur-[1px]"
+          className="w-full h-full"
         />
       </section>
     </main>
